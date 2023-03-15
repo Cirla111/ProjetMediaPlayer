10909e3378af8591c0571221deba42602545fc8e
James Alric <alric.james@ecl21.ec-lyon.fr>
2023-03-15 10:45:59 +0100

    New Animation

--- hello/routes/listen.js
+++ hello/routes/listen.js
@@ -1,18 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('listen', { title: 'Express' });
 });
-
-
-
-function redirection() {
-  
-}
-
-
 
 
 module.exports = router;
