458112f44cb0b753622aec0a1f02aa971f797f75
g64damien <damien.carrasquet@protonmail.com>
2023-03-16 19:00:15 +0100

    Liste Artiste
    Récupère la liste des artistes dans un dossier et les affiche sur la page

--- hello/routes/index.js
+++ hello/routes/index.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const folderName = '/home/damien/Musique';
+
+try {
+  if (!fs.existsSync(folderName)) {
+    console.log("Le dossier existe pas");
+    fs.mkdirSync(folderName);
+  }
+} catch (err) {
+  console.log("COUCOUCCCC");
+  console.error(err);
+}
+var list_folder= fs.readdirSync(folderName);
+var length_folder= list_folder.length;
+for (let i=0; i<length_folder; i++){
+  
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', liste: list_folder});
 });
 
 
 
+<<<<<<< Updated upstream
 function redirection() {
   
 }
@@ -16,3 +34,6 @@ function redirection() {
 
 
 module.exports = router;
+=======
+module.exports = router;
+>>>>>>> Stashed changes
